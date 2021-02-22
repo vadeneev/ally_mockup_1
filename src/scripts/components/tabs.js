aef3aba1c34057fe9ce09bb5a0433a96c036ab41
Daniil Vadeneev <daniil.vadeneyev@instamart.ru>
2021-02-22 16:49:31 +0400

    Add tabs feature.

--- src/scripts/components/tabs.js
+++ src/scripts/components/tabs.js
@@ -1,18 +1,41 @@
 export class Tabs {
+    tabsBtnElements = [];
+    tabsContentElements = [];
     tabsWrapperElement = null;
-    tabsElementsSelector = '';
 
     constructor(options) {
-        const { tabsWrapperElement, tabsElementsSelector } = options;
-        this.tabsElementsSelector = tabsElementsSelector;
+        const { tabsBtnElements, tabsContentElements, tabsWrapperElement } = options;
+        this.tabsBtnElements = tabsBtnElements;
         this.tabsWrapperElement = tabsWrapperElement;
+        this.tabsContentElements = tabsContentElements;
     }
 
-    init() {
-
+    init = () => {
+        this.tabsWrapperElement.addEventListener('click', this.handleTabBtnClick);
     }
 
     destroy() {
+        this.tabsWrapperElement.removeEventListener('click', this.handleTabBtnClick);
+    }
 
+    handleTabBtnClick = ({ target }) => {
+        if (!this.tabsBtnElements.some(element => element === target)) {
+            return;
+        }
+
+        this.tabsBtnElements.forEach((element) => {
+            element.setAttribute('aria-selected', target === element);
+            element.classList.toggle('btn--clean', target === element);
+            element.classList.toggle('btn--passive', target === element);
+        });
+
+        const tabContentId = target.getAttribute('aria-controls');
+
+        this.tabsContentElements.forEach((element) => {
+            const isCurrent = tabContentId === element.id;
+
+            element.setAttribute('aria-hidden', !isCurrent);
+            element.classList.toggle('hidden', !isCurrent);
+        });
     }
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,11 @@
 import * as loginModal from './features/login-modal-feature'
 import * as subscribeFeature from './features/subscribe-feature'
+import * as eventsTabs from './features/events-tabs'
 
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
     loginModal.init();
     subscribeFeature.init();
+    eventsTabs.init();
 }
